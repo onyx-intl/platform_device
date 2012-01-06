aa57dcac0d21a67c864f86907fa7ee2ebbe887ef
jeazh <jeazh@onyx-international.com>
2012-01-06 19:03:25 +0800

    Add new change to repo

--- fsl/imx50_rdp/AndroidBoard.mk
+++ fsl/imx50_rdp/AndroidBoard.mk
@@ -4,6 +4,5 @@ ifeq ($(PREBUILT_FSL_IMX_CODEC),true)
 include device/fsl/proprietary/codec/fsl-codec.mk
 endif
 
-include external/tslib/tslib.mk
 include device/fsl/proprietary/omx/fsl-omx.mk
 include device/fsl/proprietary/gpu/fsl-gpu.mk
